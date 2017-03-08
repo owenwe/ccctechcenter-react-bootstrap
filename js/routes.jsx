a3f062fa1c69481fbe314efabe388bd49285f605
Wes Owen <wowen@ccctechcenter.org>
2017-03-07 16:52:51 -0800

    feature/1
    resolves ddvorak/ccctechcenter-react-bootstrap#1 tested with rewrite rules in Apache 2.4 running in a docker container

--- js/routes.jsx
+++ js/routes.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { Layout } from './components/Layout'
 import Landing from './components/ui/Landing'
 import Terms from './components/containers/Terms'
 import Actions from './components/containers/Actions'
 
 const routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={Landing} />
       <Route path='terms' component={Terms} />
